f065ab72321af528878f71674c68ce3764d6835d
Fian222 <fianakhmat@gmail.com>
2024-04-04 10:37:08 +0700

    Menambahkan fungsi pada kontak

--- js/katalog.js
+++ js/katalog.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
   const produk = $('#produk');
 
-  // Array of image paths
   const gambarTenuns = ['image/tenunBulu1.png', 'image/tenunBulu2.png', 'image/tenunBulu3.png', 'image/tenunBulu4.png', 'image/tenunBulu5.png', 'image/tenunBulu6.png', 'image/tenunBulu7.png', 'image/tenunBulu8.png', 'image/tenunBulu9.png'];
 
-  // Loop through each image path and create card elements
   $.each(gambarTenuns, function (index, gambar) {
     const cardCol = $('<div class="col"></div>');
 
@@ -24,10 +22,10 @@ $(document).ready(function () {
 
     const cardPrice = $('<p class="fs-5 fw-semibold text-gradient">Rp. 630.000,00</p>');
 
-    const buyLink = $('<a class="btn btn-success px-4 h-100">Beli</a>').attr('href', 'https://api.whatsapp.com/send?phone=082130105810');
+    const button = $('<a class="btn btn-success px-4 h-100">Beli</a>').attr('href', 'https://api.whatsapp.com/send?phone=082130105810');
 
     cardDetails.append(cardPrice);
-    cardDetails.append(buyLink);
+    cardDetails.append(button);
 
     cardBody.append(cardTitle);
     cardBody.append(cardDetails);
